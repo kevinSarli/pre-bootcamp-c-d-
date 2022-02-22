72532db8d658d13d379f2c743ed30061dc881b7a
kevinSarli <kevin.sarli99@gmail.com>
2022-02-22 20:45:05 -0300

    desafio codigo

--- crear variables.js
+++
@@ -1,4 +0,0 @@
-var altura = "1 metro";//string,altura minima: 1 metro
-
-var edad = 10; //number,edad minima:10
-
---
+++ crearVariables.js
@@ -0,0 +1,8 @@
+var altura = 55;//string,altura minima: 1 metro
+
+var edad = 10; //number,edad minima:10
+
+if(altura >= 52){
+   console.log("¡Súbete, chico!") 
+}else{
+    console.log("Lo siento, chico. Tal vez el proximo año") }
